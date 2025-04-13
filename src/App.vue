b826c60688991c916365b9c5adca78b46f67de22
ann-ngambi <ngambiann08@gmail.com>
2025-04-13 09:34:15 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      name: 'Hashi car Sales',
+      status: 'pending',
+      tasks: ['task one', 'Task two', 'Task three'],
+      link: 'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status == 'active') {
+        this.status = 'pending';
+      } else if (this.status == 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ name }}</h1>
+  <br>
+  <p v-if="status == 'active'">User is active</p>
+  <p v-else-if="status == 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+<br>
+  <h3>Tasks:</h3>
+  <br>
+  <ul>
+    <li v-for="task in tasks" :key="Tasks">{{ task }}</li>
+  </ul>
+  <br>
+  <!--a v-bind:href="link">Click for google!</a-->
+  <a :href="link">Click for Google</a>
+  <br>
+  <!--button v-on:click="toggleStatus">Change status</button-->
+  <button @click="toggleStatus">Change status</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
