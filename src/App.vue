fc954b7351d6942697a8195f593e4c547e8fe616
ann-ngambi <ngambiann08@gmail.com>
2025-08-12 21:33:28 +0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,23 @@ import { RouterView } from 'vue-router';
   <Navbar/>
     <RouterView/>
 </template>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Roboto",sans-serif;
+  font-weight: 400;
+}
+.app{
+min-height: 100vh;
+position: relative;
+background-color: #f1f1f1;
+}
+.container{
+  padding: 0 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,10 +1,11 @@
 <script>
+
 </script>
 <template>
     <CNavbar color-scheme="light" class="bg-light">
   <CContainer fluid>
     <CNavbarBrand href="#">
-      <img src="/public/images/navbarLogo.svg" alt="" width="100" height="40" class="d-inline-block align-centre"/> Hashi Cars
+      <img src="/images/navbarLogo.svg" alt="" width="83" height="83" class="d-inline-block align-centre"/> Hashi Cars
     </CNavbarBrand>
   </CContainer>
 </CNavbar>
